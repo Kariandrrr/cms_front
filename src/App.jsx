fb41406348fee0c356b52c61907fb4e69be21f06
Kariandrrr <educationkarina08@gmail.com>
2026-02-16 13:37:31 +0300

    add pages and paths to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import Dashboard from './pages/admin/Dashboard.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './routes/ProtectedRoute.jsx';
 
-function App() {
-  return (
+
+import Login from './pages/Login.jsx';
+import AdminLayout from './layouts/AdminLayout.jsx';
+import Dashboard from './pages/admin/Dashboard.jsx';
+import ArticlesList from './pages/admin/ArticlesList.jsx';
+import PublicHome from './pages/public/Home.jsx';
+import ArticleView from './pages/public/ArticleView.jsx';
+
+export default function App() {
+    {return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/" element={
-          <div>
-            <h1>Публичная главная страница CMS</h1>
-            <a href="/login">Войти в админ-панель</a>
-          </div>
-        } />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/admin/*" element={<AdminLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="articles" element={<ArticlesList />} />
+
+          </Route>
+        </Route>
+        <Route path="/" element={<PublicHome />} />
+        <Route path="/articles/:slug" element={<ArticleView />} />
       </Routes>
+
     </BrowserRouter>
-  )
+  );
+}
 }
 
-export default App
+
+
+
+
