c1e625572e94fd7d4bef0baffbe606b452cc7362
Kariandrrr <educationkarina08@gmail.com>
2026-03-06 17:24:51 +0300

    little fix

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || '';
 
 const api = axios.create({
-  baseURL: API_URL || '/api',  // URL backend
+  baseURL: API_URL || '/api',
     headers: {
       'Content-Type': 'application/json',
     }
 });
 
-//  для добавления токена
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -28,7 +28,7 @@ api.interceptors.response.use(
       localStorage.removeItem('token');
       localStorage.removeItem('user');
       if (window.location.pathname !== '/login') {
-           window.location.href = '/login';  // Redirect на login
+           window.location.href = '/login';
       }
     }
     return Promise.reject(error);
