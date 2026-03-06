7009be6402b65afc8c9ce7a1f80529253b3158e4
Kariandrrr <educationkarina08@gmail.com>
2026-03-06 17:25:50 +0300

    api posts

---
+++ src/api/api_posts.jsx
@@ -0,0 +1,22 @@
+import api from '../api/axiosinstance';
+
+export const postsAPI = {
+  getPosts: (params) => api.get('/posts/', { params }),
+
+  createPost: (data) => api.post('/posts/', data),
+
+  getMyPosts: (params) => api.get('/posts/my', { params }),
+
+  getArchive: (params) => api.get('/posts/archive', { params }),
+
+  getPostById: (id) => api.get(`/posts/${id}`),
+
+  updatePost: (id, data) => api.put(`/posts/${id}`, data),
+
+  deletePost: (id) => api.delete(`/posts/${id}`),
+
+  publishPost: (id) => api.patch(`/posts/${id}/publish`),
+
+  getPostBySlug: (slug) => api.get(`/posts/slug/${slug}`),
+};
+export default postsAPI;
