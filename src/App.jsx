2e0981ddfc5e078c202ee0ae73754f1d81b4b1fd
Kariandrrr <educationkarina08@gmail.com>
2026-03-06 17:25:58 +0300

    add new routers

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/admin/Dashboard';
 import ProtectedRoute from './routes/ProtectedRoute';
+import Posts from './pages/admin/Posts';
+import EditPost from './pages/admin/EditPost';
+import CreatePost from './pages/admin/CreatePost';
+
+
 
 function App() {
   return (
@@ -25,8 +30,34 @@ function App() {
             }
           />
 
+          <Route
+          path="/admin/posts"
+          element={
+            <ProtectedRoute roles={['admin', 'editor', 'user']}>
+              <Posts />
+            </ProtectedRoute>
+          }
+          />
+
+          <Route
+            path="/admin/posts/:id/edit"
+            element={
+              <ProtectedRoute roles={['admin', 'editor', 'user']}>
+                <EditPost />
+              </ProtectedRoute>
+            }
+/>
+            <Route
+            path="/admin/posts/new"
+            element={
+              <ProtectedRoute roles={['admin', 'editor', 'user']}>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+
           {/* Редиректы */}
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
