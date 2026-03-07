61f946e917df55740db59a95a8a87d627886d837
Kariandrrr <educationkarina08@gmail.com>
2026-03-07 10:14:54 +0300

    delete some unused files

--- src/pages/public/ArticleView.jsx
+++
@@ -1,3 +0,0 @@
-export default function ArticleView() {
-  return <div>Страница простмотра статей (заглушка)</div>;
-}
--- src/pages/public/Home.jsx
+++
@@ -1,3 +0,0 @@
-export default function PublicHome() {
-  return <div>Страница дома (заглушка)</div>;
-}
